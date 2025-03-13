f489b8225f7ce47666d5b544a686d3b8ba373816
Akash Jan <116725881+akashJan@users.noreply.github.com>
2025-03-13 15:29:19 +0530

    Applied Styling

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ addition.addEventListener("click", () => {
   const numTwo = +document.querySelector("#inputFiledTwo").value;
   alert(numOne + numTwo);
 });
+
+const subtraction = document.querySelector("#subtraction");
+subtraction.addEventListener("click", () => {
+  const numOne = +document.querySelector("#inputFiledOne").value;
+  const numTwo = +document.querySelector("#inputFiledTwo").value;
+  alert(numOne - numTwo);
+});
+
+const multiplication = document.querySelector("#multiplication");
+multiplication.addEventListener("click", () => {
+  const numOne = +document.querySelector("#inputFiledOne").value;
+  const numTwo = +document.querySelector("#inputFiledTwo").value;
+  alert(numOne * numTwo);
+});
+
+const division = document.querySelector("#division");
+division.addEventListener("click", () => {
+  const numOne = +document.querySelector("#inputFiledOne").value;
+  const numTwo = +document.querySelector("#inputFiledTwo").value;
+  alert(numOne / numTwo);
+});
